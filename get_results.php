d21b02b1c8884c90c04a4f1645a671051fd0c247
lgcn <lgcn@rediffmail.com>
2019-08-03 15:43:29 +0530

    Update get_results.php

--- get_results.php
+++ get_results.php
@@ -1,20 +1,74 @@
 
+<br/><br/><br/><br/><br/>
 <?php
-if(isset($_POST['search']))
-{
- $host="localhost";
- $username="mallcom_global";
- $password="Jagdish1111#";
- $databasename="mallcom_global";
- $connect=mysql_connect($host,$username,$password);
- $db=mysql_select_db($databasename);
+    $servername = "localhost";
+    $username = "mallcom_global";
+    $password = "Jagdish111#";
 
- $search_val=$_POST['search_term'];
-	
- $get_result = mysql_query("select * from search where MATCH(title,description) AGAINST('$search_val')");
- while($row=mysql_fetch_array($get_result))
- {
-  echo "<li><a href='http://globalweblive.com/webtricks/".$row['link']."'><span class='title'>".$row['title']."</span><br><span class='desc'>".$row['description']."</span></a></li>";
- }
- exit();
-}
+// Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Results ";
+?>
+
+<br/><br/>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Search results</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<?php
+    $query = $_GET['query']; 
+    // gets value sent over search form
+     
+    $min_length = 3;
+    // you can set minimum length of the query if you want
+     
+    if(strlen($query) >= $min_length){ // if query length is more or equal minimum length then
+         
+        $query = htmlspecialchars($query); 
+        // changes characters used in html to their equivalents, for example: < to &gt;
+         
+        $query = mysql_real_escape_string($query);
+        // makes sure nobody uses SQL injection
+         
+        $raw_results = mysql_query("SELECT * FROM articles
+            WHERE (`title` LIKE '%".$query."%') OR (`text` LIKE '%".$query."%')") or die(mysql_error());
+             
+        // * means that it selects all fields, you can also write: `id`, `title`, `text`
+        // articles is the name of our table
+         
+        // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
+        // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
+        // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
+         
+        if(mysql_num_rows($raw_results) > 0){ // if one or more rows are returned do following
+             
+            while($results = mysql_fetch_array($raw_results)){
+            // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
+             
+                echo "<p><h3>".$results['title']."</h3>".$results['text']."</p>";
+                // posts results gotten from database(title and text) you can also show id ($results['id'])
+            }
+             
+        }
+        else{ // if there is no matching rows do following
+            echo "No results";
+        }
+         
+    }
+    else{ // if query length is less than minimum
+    
+                echo "Minimum length of the quers should be ".$min_length;
+    }
+?>
+</body>
+</html>
